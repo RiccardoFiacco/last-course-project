aab6cb7516bba5149ba9a0a55fd1abc02b879c6c
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-04-25 23:07:33 +0200

    add service class

---
+++ final-project/src/main/java/com/example/demo/final_project/service/CharacterService.java
@@ -0,0 +1,34 @@
+package com.example.demo.final_project.service;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.final_project.model.Character;
+import com.example.demo.final_project.repository.CharacterRepository;
+
+@Service
+public class CharacterService {
+    // classe di servizio per la gestione dei personaggi
+    // contiene metodi per le operazioni CRUD sui personaggi
+    // utilizza il repository CharacterRepository per l'accesso ai dati
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    public Character createCharacter(Character character) {
+        // crea un nuovo personaggio e lo restituisce
+        return characterRepository.save(character);
+    }
+
+    public void deleteCharacter(Integer id) throws Exception {
+        // elimina un personaggio in base all'ID
+        if (!characterRepository.existsById(id)) {
+            throw new Exception();
+        }
+        characterRepository.deleteById(id);
+    }
+
+    public Optional<Character> getCharacterById(Integer id) throws Exception {
+        // restituisce un personaggio in base all'ID
+        return characterRepository.findCharacterById(id);
+    }
+}
---
+++ final-project/src/main/java/com/example/demo/final_project/service/MangaService.java
@@ -0,0 +1,73 @@
+package com.example.demo.final_project.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.final_project.model.Character;
+import com.example.demo.final_project.model.Manga;
+import com.example.demo.final_project.repository.MangaRepository;
+
+@Service
+public class MangaService {
+    // classe di servizio per la gestione dei manga
+    // contiene metodi per le operazioni CRUD sui manga
+    // utilizza il repository MangaRepository per l'accesso ai dati
+
+    @Autowired
+    private MangaRepository mangaRepository;
+
+    public List<Manga> getAllMangas() {
+        // restituisce la lista di tutti i manga
+        return mangaRepository.findAll();
+    }
+
+    public Manga getMangaById(Integer id) throws Exception {
+        // restituisce un manga in base all'ID
+        Optional<Manga> manga = mangaRepository.findById(id);
+        if (manga.isEmpty()) {
+            throw new Exception(); // restituisce il manga se esiste
+        }
+        return manga.get();
+    }
+
+    public Optional<Manga> findMangaById(Integer id) {
+        // restituisce un manga in base all'ID
+        return mangaRepository.findById(id); // restituisce il manga se esiste
+    }
+
+    public Manga createManga(Manga manga) {
+        // crea un nuovo manga e lo restituisce
+        for (Character p : manga.getCharacters()) {
+            p.setManga(manga);
+        }
+        return mangaRepository.save(manga);
+    }
+
+    public Manga updateManga(Integer id, Manga manga) throws Exception {
+        // aggiorna un manga esistente e lo restituisce
+        Optional<Manga> existingManga = mangaRepository.findById(id);
+        if (existingManga.isEmpty()) {
+            throw new Exception();
+        }
+        manga.setId(id);
+        for (Character p : manga.getCharacters()) {
+            p.setManga(manga);
+        }
+        return mangaRepository.save(manga);// restituisce il manga aggiornato se esiste
+    }
+
+    public void deleteManga(Integer id) throws Exception {
+        // elimina un manga in base all'ID
+        Optional<Manga> existingManga = mangaRepository.findById(id);
+        if (existingManga.isEmpty()) {
+            throw new Exception();
+        }
+        mangaRepository.deleteById(id); // elimina il manga se esiste
+    }
+
+    public List<Manga> searchMangaByTitle(String title) {
+        // restituisce una lista di manga in base al titolo
+        return mangaRepository.findByTitleContainingIgnoreCase(title);
+    }
+}
